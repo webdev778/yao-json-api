56f3e09384bc6dc87654f3a4abea04f172acb4d4
kkh778 <goldrush778@yandex.com>
2017-08-04 07:43:42 +0800

    manual changed

--- test/api-test.js
+++ test/api-test.js
@@ -4,12 +4,20 @@ const yao1 = new YaoApi()
 
 async function test1(){
 
+  /*
   const { categories, error } = yao1.assetData(1).then(categories => {
     console.log(categories)
     console.log('------------------------*********************+++++++++++++++++++++++++++')
     console.log(categories.categories[0].subcategories[0].items);
   })
   .catch(error => {console.log(error)})
+  */
+
+  let assetData = await yao1.assetData(1)
+
+  console.log(assetData)
+
+  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 }
 
 test1()
